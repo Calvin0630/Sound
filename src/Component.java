b99ea770553f5a266223654e685edc0c47a7cd28
Calvin0630 <cg7057705@gmail.com>
2015-08-09 12:01:38 -0400

    added a library

--- src/Component.java
+++ src/Component.java
@@ -1,13 +1,12 @@
 import java.awt.Graphics;
+import org.jtransforms.fft.DoubleFFT_1D;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-
 import javax.swing.JComponent;
 
 
 public class Component extends JComponent {
-
-
+	DoubleFFT_1D fftDo = new DoubleFFT_1D(17460);
 	public Component() {
 		super();
 	}
@@ -16,10 +15,16 @@ public class Component extends JComponent {
 		Graphics2D g2 = (Graphics2D) g;
 		if (Main.targetData != null) {
 			byte[] soundArray = Main.targetData;
-			Rectangle[] rects = new Rectangle[soundArray.length];
+			
+			double[]wave = new double[soundArray.length];
+			for (int i=0;i<soundArray.length;i++) {
+				wave[i] = (double) soundArray[i];
+			}
+			fftDo.realForward(wave);
+			Rectangle[] rects = new Rectangle[wave.length];
 			int rectHeight;
 			for (int i=0;i<17640;i+=9) {
-				rectHeight = soundArray[i];
+				rectHeight = (int)  wave[i];
 				if (rectHeight >= 0) rects[i] = new Rectangle(i/9, 500, 1, rectHeight);
 				else rects[i] = new Rectangle(i/9, 500 + rectHeight, 1, -rectHeight);
 				g2.draw(rects[i]);
